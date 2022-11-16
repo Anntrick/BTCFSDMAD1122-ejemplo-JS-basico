4ab22de29cb2f2bb730b7c76ed5c32237decfac9
Anntrick <anntrick91@gmail.com>
2022-11-16 13:22:59 +0100

    añadido el ejercicio 10 de vanilla JS I

--- js/main.js
+++ js/main.js
@@ -151,5 +151,29 @@
 
 // let rep = prompt("Cuantas veces lo repetimos?")
 // for(let i = 0; i <= rep; i++){
+//     if(num == 5){
+//         break;
+//     }
 //     console.log(num + " X " + i + " = " + num * i);
 // }
+
+
+/* Ejercicio 10 JS vanilla I */
+// //preguntamos la cantidad de ventas que se han hecho
+// let numeroVentas = prompt("¿Cuantas ventas has hecho?");
+
+// //Inicializamos una variable para sumar el total de cada venta
+// let suma = 0;
+
+// //Recorremos cada una de las ventas en un bucle
+// //la condición en este caso será que el contador del bucle sea menor o igual al número de ventas
+// for(let i = 1; i <= numeroVentas; i++){
+//     //Por cada venta hecha, preguntamos el total
+//     let total = parseFloat(prompt("Dime el total de la venta " + i));
+//     //sumamos el total que nos han dado a la variable venta
+//     suma += total;
+//     //suma = suma + total;
+// }
+
+// //Una vez que hemos terminado de sumar las ventas, mostramos el resultado
+// console.log("Has ganado un total de: " + suma);
